305c3c29273e07e334bd9139fd5dd9fb66886729
Bryce Butler <butle2bl@dukes.jmu.edu>
2017-04-19 19:38:23 -0400

    Add files via upload

---
+++ IKM Projects/hello.go
@@ -0,0 +1,9 @@
+package main 
+
+import "fmt"
+
+//this is a comment
+
+func func main() {
+	fmt.Println("Hello, WORLD!")
+}
---
+++ IKM Projects/hello.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+			//prints "Hello, WORLD!" in the terminal window.
+			System.out.println("Hello, WORLD!");
+		}
+}
